fb57aa8c243fd826631f8b7bb000396b3d7ddccb
Mickael <mickael.spilmont@hospimedia-groupe.fr>
2020-03-03 13:24:44 +0100

    Player move with no persistence in the screen

--- engine.py
+++ engine.py
@@ -1,17 +1,21 @@
 import tcod as libtecod
 
 from input_handlers import handle_keys
+from entity import Entity
 
 def main():
   screen_width = 80
   screen_height = 50
 
-  player_x = int(screen_width / 2)
-  player_y = int(screen_height / 2)
+  player = Entity(int(screen_height / 2), int(screen_height / 2), '@', libtecod.white)
+  npc = Entity(int(screen_height / 2 - 5), int(screen_height / 2), '@', libtecod.yellow)
+  entities = [npc, player]
 
   libtecod.console_set_custom_font('arial10x10.png', libtecod.FONT_TYPE_GREYSCALE | libtecod.FONT_LAYOUT_TCOD)
 
   libtecod.console_init_root(screen_width, screen_height, 'roguelike_python_tcod', False)
+
+  con = libtecod.console_new(screen_width, screen_height)
 
   key = libtecod.Key()
   mouse = libtecod.Mouse()
@@ -19,9 +23,15 @@ def main():
   while not libtecod.console_is_window_closed():
     libtecod.sys_check_for_event(libtecod.EVENT_KEY_PRESS, key, mouse)
 
+    libtecod.console_set_default_foreground(con, libtecod.white)
+    libtecod.console_put_char(con, player.x, player.y, '@', libtecod.BKGND_NONE)
+    libtecod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
     libtecod.console_set_default_foreground(0, libtecod.white)
-    libtecod.console_put_char(0, player_x, player_y, '@', libtecod.BKGND_NONE)
+    libtecod.console_put_char(0, player.x, player.y, '@', libtecod.BKGND_NONE)
     libtecod.console_flush()
+
+    libtecod.console_put_char(con, player.x, player.y, ' ', libtecod.BKGND_NONE)
+    libtecod.console_put_char(0, player.x, player.y, ' ', libtecod.BKGND_NONE)
 
     action = handle_keys(key)
 
@@ -31,8 +41,7 @@ def main():
 
     if move:
       dx, dy = move
-      player_x += dx
-      player_y += dy
+      player.move(dx, dy)
 
     if exit:
       return True
